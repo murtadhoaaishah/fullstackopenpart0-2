00e91d4426da96d7d224699a5bd0f182725abfae
hemsleek <asiyanbimubashir@gmail.com>
2021-07-09 16:49:10 +0100

    feat:structure and part0

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,18 +1,20 @@
+const Hello = () => {
+    return (
+      <div>
+        <p>Hello world</p>
+      </div>
+    )
+  }
+  
+  const App = () => {
+    return (
+      <div>
+        <h1>Greetings</h1>
+        <Hello />
+      </div>
+    )
+  }
 
-    const Hello = () => {
-        return (
-          <div>
-            <p>Hello world</p>
-          </div>
-        )
-      }
-      
-      const App = () => {
-        return (
-          <div>
-            <h1>Greetings</h1>
-            <Hello />
-          </div>
-        )
-      }
-export default App
+
+
+
